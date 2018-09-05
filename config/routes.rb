3767982aa04a4df869ada1cb0dcb7b0bb1129f16
Geoff Vincent <geoffkvincent@gmail.com>
2018-09-05 16:54:51 -0600

    added billboard controller, index and create

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'billboards#index'
+
+  resources :billboards
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
